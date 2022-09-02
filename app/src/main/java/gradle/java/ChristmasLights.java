eba149073e01946ac5c6f11ac9b3171a365fae5a
Julieta Martin <julieta.martin@clarity.ai>
2022-09-02 15:10:14 +0200

    logic part in progress

--- app/src/main/java/gradle/java/ChristmasLights.java
+++ app/src/main/java/gradle/java/ChristmasLights.java
@@ -2,5 +2,27 @@ package gradle.java;
 
 public class ChristmasLights {
 
+    Integer turnOn = 1;
+    Integer turnOff = 0;
+    Integer[][] initialGrid = {{0,0,0}, {0,0,0}, {0,0,0}};
 
+
+    public boolean toggleGrid(Integer fila1, Integer columna1, Integer fila2, Integer columna2) {
+
+        for (int i = 0; i < initialGrid.length; i++) {
+            for (int j = 0; j < initialGrid.length; j++) {
+                if(i <= fila1 && i >= fila2){
+                    if(j >= columna1 && j <= columna2) {
+                        if (initialGrid[i][j] == 0){
+                            var toogleTo = initialGrid[i][j] == 1;
+                            return toogleTo;
+                        }
+                        var toogleTo = initialGrid[i][j] == 0;
+                        return toogleTo;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 }
